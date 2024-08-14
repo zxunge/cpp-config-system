f18477ae61877d989c05d0db194b19f826bd51bf
万顷茫然 <121007356+zxunge@users.noreply.github.com>
2024-08-14 16:13:30 +0800

    Update config_system.h

--- config_system.h
+++ config_system.h
@@ -1,97 +1,104 @@
-#pragma once
+#ifndef CONFIG_SYSTEM_H_
+#define CONFIG_SYSTEM_H_
+
 #include <string>
 #include <fstream>
 #include <vector>
 
 namespace configsys
 {
-	class element 
-  {
-	public:
-		element( ) { }
-		element( std::string_view element_name, bool value ) : m_element_name( element_name )	{ m_element_string = value ? "1" : "0"; }
-		element( std::string_view element_name, double value ) : m_element_name( element_name )	{ m_element_string = std::to_string( value ); }
-		element( std::string_view element_name, float value ) : m_element_name( element_name )	{ m_element_string = std::to_string( value ); }
-		element( std::string_view element_name, int value ) : m_element_name( element_name )	{ m_element_string = std::to_string( value ); }
-		element( std::string_view element_name, const char* value ) : m_element_name( element_name ), m_element_string( value ) { }
+class element 
+{
+public:
+    element( ) {}
+    element( std::string_view element_name, bool value ) : m_element_name( element_name )	{ m_element_string = value ? "1" : "0"; }
+    element( std::string_view element_name, double value ) : m_element_name( element_name )	{ m_element_string = std::to_string( value ); }
+    element( std::string_view element_name, float value ) : m_element_name( element_name )	{ m_element_string = std::to_string( value ); }
+    element( std::string_view element_name, int value ) : m_element_name( element_name )	{ m_element_string = std::to_string( value ); }
+    element( std::string_view element_name, const char* value ) : m_element_name( element_name ), m_element_string( value ) { }
 
-		bool as_bool( )				{ return as_int( ) == 1; }
-		double as_double( )			{ return std::stod( m_element_string ); }
-		float as_float( )			{ return std::stof( m_element_string ); }
-		int as_int( )				{ return std::stoi( m_element_string ); }
-		std::string as_string( )	{ return m_element_string; }
+    bool as_bool( )				{ return as_int( ) == 1; }
+    double as_double( )			{ return std::stod( m_element_string ); }
+    float as_float( )			{ return std::stof( m_element_string ); }
+    int as_int( )				{ return std::stoi( m_element_string ); }
+    std::string as_string( )	{ return m_element_string; }
 
-		std::string get_name( )		{ return m_element_name; }
+    std::string get_name( )		{ return m_element_name; }
 
-		std::string make_string( std::uint32_t indent );
+    std::string make_string( std::uint32_t indent );
 
-	private:
-		std::string m_element_name = "", m_element_string = "";
-	};
+private:
+    std::string m_element_name = "", m_element_string = "";
+};
 
-	class element_group 
-  {
-	public:
-		element_group( ) { }
-		element_group( std::string_view name ) : m_group_name( name ) { }
-		element_group( std::string_view name, std::vector< std::string > content ) : m_group_name( name ) { parse_group( content ); }
+class element_group 
+{
+public:
+    element_group( ) { }
+    element_group( std::string_view name ) : m_group_name( name ) { }
+    element_group( std::string_view name, std::vector< std::string > content ) : m_group_name( name ) { parse_group( content ); }
 
-		void add_element( std::string_view name, bool value )				{ m_elements.push_back( element( name, value ) ); }
-		void add_element( std::string_view name, double value )				{ m_elements.push_back( element( name, value ) ); }
-		void add_element( std::string_view name, float value )				{ m_elements.push_back( element( name, value ) ); }
-		void add_element( std::string_view name, int value )				{ m_elements.push_back( element( name, value ) ); }
-		void add_element( std::string_view name, const char* value )		{ m_elements.push_back( element( name, value ) ); }
+    void add_element( std::string_view name, bool value )				{ m_elements.push_back( element( name, value ) ); }
+    void add_element( std::string_view name, double value )				{ m_elements.push_back( element( name, value ) ); }
+    void add_element( std::string_view name, float value )				{ m_elements.push_back( element( name, value ) ); }
+    void add_element( std::string_view name, int value )				{ m_elements.push_back( element( name, value ) ); }
+    void add_element( std::string_view name, const char* value )		{ m_elements.push_back( element( name, value ) ); }
 
-		element_group& create_group( std::string_view group_name );
-		element_group get_group( std::string_view group_name );
+    element_group& create_group( std::string_view group_name );
+    element_group get_group( std::string_view group_name );
 
-		element get_element( std::string_view element_name );
+    element get_element( std::string_view element_name );
 
-		std::string get_name( ) { return m_group_name; }
+    std::string get_name( ) { return m_group_name; }
 
-		std::string make_string( std::uint32_t indent );
+    std::string make_string( std::uint32_t indent );
 
-	private:
-		void parse_group( const std::vector< std::string >& content );
+private:
+    void parse_group( const std::vector< std::string >& content );
 
-		std::string m_group_name = "";
+    std::string m_group_name = "";
 
-		std::vector< element > m_elements = { };
-		std::vector< element_group > m_element_groups = { };
-	};
+    std::vector< element > m_elements = { };
+    std::vector< element_group > m_element_groups = { };
+};
 
-	class config_system 
-  {
-	public:
-		config_system( ) { }
-		~config_system( );
+class config_system 
+{
+public:
+    config_system( ) {}
+    ~config_system( );
 
-		void add_element( std::string_view name, bool value )			{ m_elements.push_back( element( name, value ) ); }
-		void add_element( std::string_view name, float value )			{ m_elements.push_back( element( name, value ) ); }
-		void add_element( std::string_view name, int value )			{ m_elements.push_back( element( name, value ) ); }
-		void add_element( std::string_view name, const char* value )	{ m_elements.push_back( element( name, value ) ); }
+    void add_element( std::string_view name, bool value )			{ m_elements.push_back( element( name, value ) ); }
+    void add_element( std::string_view name, float value )			{ m_elements.push_back( element( name, value ) ); }
+    void add_element( std::string_view name, int value )			{ m_elements.push_back( element( name, value ) ); }
+    void add_element( std::string_view name, const char* value )	        { m_elements.push_back( element( name, value ) ); }
 
-		bool open_as_read( std::string_view file );
-		bool open_as_write( std::string_view file );
+    bool open( std::string_view file );
 
-		void save( );
-		void close( );
+    void save( );
+    void close( );
 
-		element_group& create_group( std::string_view group_name );
-		element_group get_group( std::string_view group_name );
+    element_group& create_group( std::string_view group_name );
+    element_group get_group( std::string_view group_name );
 
-		element get_element( std::string_view element_name );
+    element get_element( std::string_view element_name );
 
-	private:
-		void parse_config_file( std::string_view file );
+private:
+    bool open_as_read( std::string_view file );
+    bool open_as_write( std::string_view file );
 
-		std::fstream m_stream = { };
+    void parse_config_file( std::string_view file );
 
-		std::vector< element > m_elements = { };
-		std::vector< element_group > m_element_groups = { };
+    std::fstream m_readStream {};
+    std::fstream m_writeStream {};
+
+    std::vector< element > m_elements {};
+    std::vector< element_group > m_element_groups {};
       
-	};
+};
 
-	std::string get_element_attribute( const std::string& element, const std::string& attribute );
-	std::string get_element_value( const std::string& element );
+std::string get_element_attribute( const std::string& element, const std::string& attribute );
+std::string get_element_value( const std::string& element );
 }
+
+#endif
